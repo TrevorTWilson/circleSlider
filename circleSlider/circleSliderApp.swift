a828e42746dd481b7632cc0da2334081354d89b0
Trevor Wilson <trevortwilson@hotmail.com>
2024-01-20 12:31:08 -0700

    Initial Commit

---
+++ circleSlider/circleSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  circleSliderApp.swift
+//  circleSlider
+//
+//  Created by trevor wilson on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct circleSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
