fd27fd38e602fe59e21acd353fe01b1e742ba5e1
Trevor Wilson <trevortwilson@hotmail.com>
2024-01-21 14:11:44 -0700

    moved content view to default location

---
+++ circleSlider/circleSlider VC.swift
@@ -0,0 +1,18 @@
+//
+//  circleSlider VC.swift
+//  circleSlider
+//
+//  Created by trevor wilson on 2024-01-20.
+//
+
+import SwiftUI
+
+struct circleSlider_VC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    circleSlider_VC()
+}
