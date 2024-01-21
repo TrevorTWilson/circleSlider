9e2f3a1cb036724f8201c011bce0992baa054b59
Trevor Wilson <trevortwilson@hotmail.com>
2024-01-21 14:12:34 -0700

    changed to multiview controller in default location

--- circleSlider/ContentView.swift
+++ circleSlider/ContentView.swift
@@ -7,18 +7,48 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(Color.init(red: 34/255, green: 30/255, blue: 47/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    CircleSliderVC()
+                    Spacer()
+                    CircleSliderVC()
+                    Spacer()
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    CircleSliderVC()
+                    Spacer()
+                    CircleSliderVC()
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
